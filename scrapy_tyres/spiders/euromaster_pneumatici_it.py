cb9159625fb49966f9e0bc514b4e3857a94feb86
Matteo <matteo.redaelli@pirelli.com>
2017-03-27 11:25:54 +0200

    first import

---
+++ scrapy_tyres/spiders/euromaster_pneumatici_it.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import datetime, re
+
+class EuromasterPneumaticiItSpider(scrapy.Spider):
+    name = "euromaster-pneumatici.it"
+    allowed_domains = ["euromaster-pneumatici.it"]
+    start_urls = [
+            'https://www.euromaster-pneumatici.it/pneumatico'
+            ]
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    
+    def parse(self, response):
+        for url in response.xpath('//div[@class="marque"]/a/@href').extract():
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_brand)
+
+    def parse_brand(self, response):
+        # response.xpath('//div[@class="marque"]/a/@href').extract()
+        for url in response.xpath('//ul[@class="listProfileDimension"]/li/a/@href').extract():
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_tyres)
+
+    def parse_tyres(self, response):
+        for item in response.xpath('//div[@data-idproduct="1"]'):
+            
+            brand = item.xpath('.//img[@class="produit-visu-manuf"]/@alt').extract_first()
+            if not brand:
+                continue
+            brand = brand.strip()
+            product = item.xpath('.//div[@class="produit-desc"]/strong/text()').extract_first().strip()
+            
+            season = item.xpath('.//div[@class="produit-desc"]/strong/img/@alt').extract_first().replace("Pneumatico ", "")
+            match = re.match("^(.+) (.+)$", season)
+            if not match:
+                continue
+            veycle = match.groups()[0]
+            season = match.groups()[1]
+
+            url = item.xpath('./a/@href').extract_first()
+            match = re.match(".+/.+/.+/(.+)--(.+)$", url)
+            size = ""
+            description = ""
+            if match:
+                size = match.groups()[0]
+                description = match.groups()[1]
+            
+            id = item.xpath('.//div[@class="produit-visu"]/img/@id').extract_first()
+            match = re.match("^.+-(.+)$", id)
+            if match:
+                id = match.groups()[0]
+            else:
+                id=""
+
+            price = item.xpath('.//div[@class="produit-prix "]/strong/text()').extract_first()
+            
+            yield {
+                    "brand": brand,
+                    "day": self.today,
+                    "description": description,
+                    "id": id,
+                    "price": price,
+                    "product": product,
+                    "season": season,
+                    "site": self.name,
+                    "size": size,
+                    "url": response.urljoin(url),
+                    "veycle": veycle
+                    }
---
+++ scrapy_tyres/spiders/norauto_it.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import datetime, re
+
+def clean_text(text):
+    t = re.sub("[\t\n\r]", " ", text)
+    return re.sub(" +", " ", t).strip()
+
+class NorautoItSpider(scrapy.Spider):
+    name = "norauto.it"
+    allowed_domains = ["norauto.it"]
+    start_urls = ['https://www.norauto.it/0/0/0/0/0/0-0-0.html?PageNumber=1']
+    today = datetime.date.today().strftime("%Y-%m-%d")
+
+    def parse(self, response):
+        for item in response.xpath('//div[@class="ws-seg blc-all"]//div[@class="product-item-visible"]'):
+            brand = item.xpath('.//div[@class="brand-logo"]//img/@alt').extract_first() 
+            product = item.xpath('.//div[@class="product-info"]//a/text()').extract_first().replace('Pneumatico ', "")
+            url = item.xpath('.//div[@class="product-info"]//a/@href').extract_first()
+            match = re.match('.*_(.+)\.html$', url)
+            if match:
+                id = match.group(1)
+            else:
+                id = ""
+            description = item.xpath('.//div[@class="product-info"]//p/text()').extract_first()
+            season = item.xpath('.//div[@class="product-weather"]/a/span/@class').extract_first()
+            price = item.xpath('.//div[@class="product-price"]//span[@itemprop="price"]/text()').extract_first()
+            yield {
+                    "brand": brand,
+                    "day": self.today,
+                    "description": clean_text(description),
+                    "id": id,
+                    "product": clean_text(product),
+                    "price": price,
+                    "season": season,
+                    "site": self.name,
+                    "url": url,
+                    }
+        next_page = response.xpath('//a[@class="next"]/@href').extract_first()
+        if next_page:
+            yield scrapy.Request(next_page, callback=self.parse)
