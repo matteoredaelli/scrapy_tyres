ff6e60a6ce49ab62a76c0ec4eef04d0fb3e71a5b
Matteo <matteo.redaelli@pirelli.com>
2017-12-21 15:35:53 +0100

    flask run

--- ws.py
+++ ws.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__,  static_url_path='www/')
 
 import tyre_utils
 import utils
@@ -9,7 +9,12 @@ import json
 
 VERSION=0.1
 
+
 @app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/version')
 def version():
     return 'Tyre info extractor version %s' % VERSION
 
@@ -22,6 +27,7 @@ def extract_info():
         item = {"description": utils.clean_text(description.upper())}
         result = tyre_utils.mergeItems(item, tyre_utils.extractAll(item))
     return json.dumps(result)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
