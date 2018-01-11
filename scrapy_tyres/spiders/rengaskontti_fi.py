bfd6d5ccecf3c235b5b404273e8f9b32a1346108
Matteo <matteo.redaelli@pirelli.com>
2018-01-11 17:53:39 +0100

    fix in util module

--- scrapy_tyres/spiders/rengaskontti_fi.py
+++ scrapy_tyres/spiders/rengaskontti_fi.py
@@ -21,7 +21,8 @@
 
 import scrapy
 import datetime, re
-import utils, tyre.utils
+import utils
+from tyre import utils as tyre_utils
 
 class RengaskonttiFi(scrapy.Spider):
     name = "rengaskontti.fi"
@@ -29,7 +30,7 @@ class RengaskonttiFi(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super(RengaskonttiFi, self).__init__(*args, **kwargs)
         self.allowed_domains = ["rengaskontti.fi"]
-        self.brands = tyre.utils.load_brands()
+        self.brands = tyre_utils.load_brands()
         self.brands = [s.replace("-", "_").replace(" ", "_") for s in self.brands]
      
         self.start_urls = ['https://www.rengaskontti.fi/auton-renkaat/%s/' % x for x in self.brands] 
--- tyre/utils.py
+++ tyre/utils.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
 import logging
+import utils
+
 ##
 ## isXXX
 ##
